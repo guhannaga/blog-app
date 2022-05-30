6b6036a10d2e8fc49321bdf30740a6078aba667b
guhannaga <guhan.ult@gmail.com>
2022-05-30 15:44:54 +0530

    Created Post entity and Implemented repository class

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,31 @@
+package com.springboot.blog.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames={"title"})})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title",nullable = false)
+    private String title;
+    @Column(name="description", nullable = false)
+    private String description;
+    @Column(name="content")
+    private String content;
+
+}
